3df68c74bcd77ed8722b6575b805ebb5fdbe19d3
Bingxing Wang <i@imbushuo.net>
2025-05-30 20:02:50 -0700

    Touchpad works?

--- src/hv_exc.c
+++ src/hv_exc.c
@@ -499,12 +499,14 @@ void hv_exc_sync(struct exc_info *ctx)
 
 void hv_exc_irq(struct exc_info *ctx)
 {
+    UNUSED(ctx);
+
     if ((mrs(ISR_EL1) & 0x80))
     {
         PERCPU(irq_reason) = read32(aic->base + aic->regs.event);
-        // printf("IRQ on CPU%d 0x%x\n", smp_id(), PERCPU(irq_reason));
         if (PERCPU(irq_reason) && !PERCPU(irq_fired))
         {
+            printf("IRQ on CPU%d 0x%x\n", smp_id(), PERCPU(irq_reason));
             u64 hcr = mrs(HCR_EL2);
             PERCPU(irq_fired) = true;
             hv_write_hcr(hcr | HCR_VI);
